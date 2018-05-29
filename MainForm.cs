b4026337a81fe2483f3ff74a6d0fd551c86fbf77
sougannkyou <songyq0509@163.com>
2018-05-29 08:56:44 +0800

    @company

--- MainForm.cs
+++ MainForm.cs
@@ -31,6 +31,7 @@ namespace NiceClip
         string myIP = "127.0.0.1";
         int copyCnt = 0;
         int copyErrorCnt = 0;
+        DateTime startTime = DateTime.Now;
 
         public MainForm()
         {
@@ -138,15 +139,15 @@ namespace NiceClip
                             Client.AddItemToSet("devices:" + this.myIP + "_org", clipboardText);
                             clipboardHistoryList.Items.Insert(0, clipboardText);
                             this.copyCnt++;
-                            toolStripStatusLabel.Text = "copy success: " + this.copyCnt.ToString();
                         }
                         catch (Exception e)
                         {
                             this.copyErrorCnt++;
-                            toolStripStatusLabel.Text = "copy failure: " + this.copyErrorCnt.ToString();
                             Console.WriteLine(e);
                         }
-                       
+
+                        toolStripStatusLabel.Text = "copy success: " + this.copyCnt.ToString() + " failure: " + this.copyErrorCnt.ToString();
+                        toolStripStatusLabel.Text += " times: " + (DateTime.Now - startTime).ToString().Substring(0,8);
                         deleteButton.Enabled = true;
                     }
                 }
